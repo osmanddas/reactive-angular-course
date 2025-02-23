fc566bd463b66e645df956899d03108ec3022d79
OPD <osmandpdas@gmail.com>
2025-02-23 11:22:37 -0500

    added localStorage capabilities for storing user profile surviving browser refresh

--- src/app/services/auth.store.ts
+++ src/app/services/auth.store.ts
@@ -4,6 +4,7 @@ import { User } from "../model/user";
 import { map, shareReplay, tap } from "rxjs/operators";
 import { HttpClient } from "@angular/common/http";
 
+const AUTH_DATA = "auth_data";
 
 @Injectable({ providedIn: 'root'})
 export class AuthStore {
@@ -20,18 +21,30 @@ export class AuthStore {
         this.isLoggedIn$ = this.user$.pipe(map(user => !!user));
         this.isLoggedOut$ = this.isLoggedIn$.pipe(map(loggedIn => !loggedIn ));
 
+        const user = localStorage.getItem(AUTH_DATA);
+        if(user){
+            console.log('Obtained user object JSON string from browser cache: ', user)
+            this.subject.next(JSON.parse(user));
+        }
+
     }
 
     login(email: string, password: string): Observable<User> {
        return this.http.post<User>("/api/login", {email, password}).pipe(
-            tap(user => this.subject.next(user)),
+            tap(user => { 
+                this.subject.next(user);
+                localStorage.setItem(AUTH_DATA, JSON.stringify(user)); 
+                // store user object JSON string in  browser cache to survive refresh
+                }
+            ),
             shareReplay()
         );
 
     }
 
     logout() {
-        this.subject.next(null)
+        this.subject.next(null);
+        localStorage.removeItem(AUTH_DATA);
     }
 
 }
